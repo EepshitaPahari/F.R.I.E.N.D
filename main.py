69afa64f525379a6f485b494579d34b7977b3209
vikashAgnihotry <vikash.kumar.ece25@heritageit.edu.in>
2025-03-03 00:11:26 +0530

    space and delete buttons are exchanged in keyboard

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def open_medication_page(username):
             '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
             'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
             'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
-            'z', 'x', 'c', 'v', 'b', 'n', 'm', ':', 'Space', 'Delete'
+            'z', 'x', 'c', 'v', 'b', 'n', 'm', ':', 'Delete', 'Space'
         ]
 
         # Set up the frame for the virtual keyboard
